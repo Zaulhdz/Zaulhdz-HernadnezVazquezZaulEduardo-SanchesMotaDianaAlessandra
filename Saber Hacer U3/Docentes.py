65424ea5adedaa663ed6d631cb3e016cf37d272d
Zaul Hernandez <zaul.utc@gmail.com>
2023-11-22 21:50:48 -0600

    Agregue al profesor Alan con POST

--- Saber Hacer U3/Docentes.py
+++ Saber Hacer U3/Docentes.py
@@ -4,6 +4,12 @@ Docentes =[
         "Especialidad": "Optativa_BI",
         "Nombre": "Patricia_Guadalupe_Mora",
         "Num. Empleado": "190002"
+    },
+    {
+        "Edificio": "II Tecnologias de la informacion",
+        "Especialidad": "Sistemas de calidad",
+        "Nombre": "Alan_de_Jesus",
+        "Num. Empleado": "190003"
     }
 ]
 
