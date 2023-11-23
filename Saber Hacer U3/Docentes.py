acae5ffa5edf58bbbeec12ced2c2656654aed3ae
unknown <20045147@alumno.utc.edu.mx>
2023-11-23 17:40:14 -0600

    Con DELETE se elimino a un Docente

--- Saber Hacer U3/Docentes.py
+++ Saber Hacer U3/Docentes.py
@@ -7,12 +7,6 @@ Docentes =[
     },
     {
         "Edificio": "II Tecnologias de la informacion",
-        "Especialidad": "Sistemas de calidad",
-        "Nombre": "Alan_de_Jesus",
-        "Num. Empleado": "190003"
-    },
-    {
-        "Edificio": "II Tecnologias de la informacion",
         "Especialidad": "Hacking_Etico",
         "Nombre": "Roberto_Carlos",
         "Num. Empleado": "190004"
