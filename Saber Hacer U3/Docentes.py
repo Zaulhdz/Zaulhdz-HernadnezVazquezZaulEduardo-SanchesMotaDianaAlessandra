056cbdaee6534f394c81775e59ae625699cd76c1
Zaul Hernandez <zaul.utc@gmail.com>
2023-11-22 21:23:57 -0600

    Con en metodo DELETE se elimono a uno de los Docentes

--- Saber Hacer U3/Docentes.py
+++ Saber Hacer U3/Docentes.py
@@ -1,12 +1,6 @@
 Docentes =[
     {
         "Edificio": "II Tecnologias de la informacion",
-        "Especialidad": "Automatizacion_de_Infraestructura_de_redes",
-        "Nombre": "Gabriel_Alejandro_Reyes",
-        "Num. Empleado": "190001"
-    },
-    {
-        "Edificio": "II Tecnologias de la informacion",
         "Especialidad": "Optativa_BI",
         "Nombre": "Patricia_Guadalupe_Mora",
         "Num. Empleado": "190002"
