7f2d12cd097c0b5d734446dfcb2ccf37cf995b45
unknown <20045147@alumno.utc.edu.mx>
2023-11-23 17:33:48 -0600

    Con PATCH se modifico a uno de los docentes

--- Saber Hacer U3/Docentes.py
+++ Saber Hacer U3/Docentes.py
@@ -2,7 +2,7 @@ Docentes =[
     {
         "Edificio": "II Tecnologias de la informacion",
         "Especialidad": "Optativa_BI",
-        "Nombre": "Patricia_Guadalupe_Mora",
+        "Nombre": "Patricia_Mora",
         "Num. Empleado": "190002"
     },
     {
