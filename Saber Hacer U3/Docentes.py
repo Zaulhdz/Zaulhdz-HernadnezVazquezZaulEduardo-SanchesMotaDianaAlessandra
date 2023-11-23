6be8f3102ded71b029f225e2a5180e7f982f5cce
unknown <20045147@alumno.utc.edu.mx>
2023-11-23 17:27:21 -0600

    se agrego a otro docnete usand POST

--- Saber Hacer U3/Docentes.py
+++ Saber Hacer U3/Docentes.py
@@ -17,6 +17,12 @@ Docentes =[
         "Nombre": "Roberto_Carlos",
         "Num. Empleado": "190004"
     },
+    {
+        "Edificio": "II Tecnologias de la informacion",
+        "Especialidad": "Direccion de Proyectos II",
+        "Nombre": "Azucena",
+        "Num. Empleado": "190005"
+    }
 ]
 
 
