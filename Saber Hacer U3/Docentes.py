11b6647be4f81148c41c63632ad90ed69305673f
Zaulhdz <zaul.utc@gmail.com>
2023-11-22 20:32:52 -0600

    Se agrego a un docente con el metodo POST

--- Saber Hacer U3/Docentes.py
+++ Saber Hacer U3/Docentes.py
@@ -1,3 +1,10 @@
-Docentes =[]
+Docentes =[
+    {
+        "Edificio": "II",
+        "Especialidad": "Automatizacion_de_Infraestructura",
+        "Nombre": "Gabriel_Alejandro",
+        "Num. Empleado": "190001"
+    }
+]
 
 
